1b93038ae4e679fd4b3a177231e57d01d372ce52
atomdog <agom@bu.edu>
2022-05-05 15:07:25 -0400

    finishing

--- startup.py
+++ startup.py
@@ -2,3 +2,13 @@
 
 import configlib
 import credLib
+import audiocortex
+import semweblib
+import time
+import perception
+
+configlib.createBlankCred()
+credLib.createBlankCred()
+semweblib.torch_web()
+audiocortex.wipe_log()
+perception.torch_stack()
